ddc07f723e8ccfa66674c81d9ea9832220c5b3fe
Kuzetov-Alexandr <107497862+Kuzetov-Alexandr@users.noreply.github.com>
2023-05-20 15:32:39 +0500

    add 4 solutions

---
+++ lib/easy/014_max_prefix.dart
@@ -0,0 +1,18 @@
+class Solution {
+  String longestCommonPrefix(List<String> strs) {
+    String res = "";
+    for (int i = 0; i < strs[0].length; i++) {
+      for (String s in strs) {
+        if (i >= s.length || strs[0][i] != s[i]) {
+          return res;
+        }
+      }
+      res += strs[0][i];
+    }
+    return res;
+  }
+}
+
+void main(List<String> args) {
+  print(Solution().longestCommonPrefix(["flower", "flow", "flight"]));
+}
